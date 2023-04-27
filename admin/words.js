729bbd8a083d2208ceec6732e4b19dbd781e8a86
Flonicar <daflow81@googlemail.com>
2023-04-27 22:06:27 +0200

    first commit

---
+++ admin/words.js
@@ -0,0 +1,46 @@
+/* eslint no-unused-vars: off */
+/* eslint no-global-assign: off */
+/* global systemDictionary */
+'use strict';
+
+systemDictionary = {
+    'enpal_solar_information adapter settings': {
+        en: 'Adapter settings for enpal_solar_information',
+        de: 'Adaptereinstellungen für enpal_solar_information',
+        ru: 'Настройки адаптера для enpal_solar_information',
+        pt: 'Configurações do adaptador para enpal_solar_information',
+        nl: 'Adapterinstellingen voor enpal_solar_information',
+        fr: "Paramètres d'adaptateur pour enpal_solar_information",
+        it: "Impostazioni dell'adattatore per enpal_solar_information",
+        es: 'Ajustes del adaptador para enpal_solar_information',
+        pl: 'Ustawienia adaptera dla enpal_solar_information',
+        uk: 'Налаштування адаптера для enpal_solar_information',
+        'zh-cn': 'enpal_solar_information的适配器设置',
+    },
+    option1: {
+        en: 'option1',
+        de: 'Option 1',
+        ru: 'Опция 1',
+        pt: 'Opção 1',
+        nl: 'Optie 1',
+        fr: 'Option 1',
+        it: 'opzione 1',
+        es: 'Opción 1',
+        pl: 'opcja 1',
+        uk: 'варіант1',
+        'zh-cn': '选项1',
+    },
+    option2: {
+        en: 'option2',
+        de: 'Option 2',
+        ru: 'вариант 2',
+        pt: 'opção 2',
+        nl: 'Optie 2',
+        fr: 'Option 2',
+        it: 'opzione 2',
+        es: 'opcion 2',
+        pl: 'Opcja 2',
+        uk: 'варіант2',
+        'zh-cn': '选项2',
+    },
+};
